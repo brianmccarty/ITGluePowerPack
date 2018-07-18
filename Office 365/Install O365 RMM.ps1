13fcb0da972d4c46570dd1336f2852b20d23b5f5
ecspresso <emile@ecspresso.com>
2018-07-18 09:33:13 +0200

    Added parameters for logging, updated inline help.

--- Office 365/Install O365 RMM.ps1
+++ Office 365/Install O365 RMM.ps1
@@ -2,17 +2,27 @@
 .SYNOPSIS
     Installs the AzureAD module in order to connect to Office 365.
 .DESCRIPTION
-
+    Visit https://github.com/UpstreamAB/ITGluePowerPack/wiki/Office-365 for help on how to use this script.
+.PARAMETER ManualMode
+    Default mode, will try to elevate and you will be prompted when installing.
+.PARAMETER Silent
+    Automatically install what is needed. No prompts. Will not try to elevate.
 .PARAMETER Username
     Your Office 365 username. This will be saved in -path\o365credentials.xml unless specifed not to.
 .PARAMETER Password
     Your Office 365 password. This will be saved in -path\o365credentials.xml unless specifed not to.
 .PARAMETER Path
     Default $env:USERPROFILE\UpstreamPowerPack. Set this parameter if you want to store your credentials somewhere else.
+.PARAMETER Log
+    Turns on logging.
+.PARAMETER LogPath
+    Location to store logPath. Default same as Path.
+.PARAMETER SaveCredentials
+    Whether or not to save credentials for later use.
 .NOTES
     Author:  Emile Priller
     Date:    08/05/2018
-    Updated: 09/07/2018
+    Updated: 18/07/2018
 #>
 [CmdletBinding(DefaultParameterSetName="ManualMode")]
 param(
@@ -36,6 +46,14 @@ param(
 
     [Parameter(ParameterSetName="ManualMode")]
     [Parameter(ParameterSetName="Silent")]
+    [Switch]$Log,
+
+    [Parameter(ParameterSetName="ManualMode")]
+    [Parameter(ParameterSetName="Silent")]
+    [string]$LogPath = "$env:USERPROFILE\UpstreamPowerPack",
+
+    [Parameter(ParameterSetName="ManualMode")]
+    [Parameter(ParameterSetName="Silent")]
     [Switch]$SaveCredentials
 )
 
@@ -44,7 +62,7 @@ Function Write-Message {
     Param ($Message, [switch]$Warning, [switch]$Throw)
 
     if($Log) {
-        Out-File -InputObject "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] $($Message)" -FilePath $LogFile -Append
+        Out-File -InputObject "[$(Get-Date -Format "yyyy-MM-dd HH:mm:ss")] $($Message)" -FilePath $LogPath -Append
     }
     if($Warning) {
         Microsoft.PowerShell.Utility\Write-Verbose -Message "$Message"
@@ -71,16 +89,6 @@ Function Save-Credentials {
     }
 }
 
-if(![bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
-    try {
-        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        Exit
-    } catch [Exception]{
-        Write-Message -Message "Please run this script again as admin." -Throw
-    }
-}
-
 if($Path.EndsWith("\")) {
     $Path = $Path.Remove($Path.Length -1, 1)
 }
@@ -93,7 +101,29 @@ if(-not (Test-Path -path $Path)) {
     }
 }
 
+if($LogPath.EndsWith("\")) {
+    $LogPath = $LogPath.Remove($LogPath.Length -1, 1)
+}
+
+if(-not (Test-Path -path $LogPath)) {
+    try {
+        New-Item $LogPath -ItemType Directory
+    } catch [Exception]{
+        Write-Message -Message $_ -Throw
+    }
+}
+
 if($ManualMode) {
+    if(![bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
+        try {
+            $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+            Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+            Exit
+        } catch [Exception]{
+            Write-Message -Message "Please run this script again as admin." -Throw
+        }
+    }
+
     Install-Module AzureAD -Force
 
     if(SaveCredentials) {
