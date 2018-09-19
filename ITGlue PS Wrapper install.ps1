5513beaeb172db73e3871f370aaf4a248eec3355
ecspresso <emile@ecspresso.com>
2018-09-19 10:27:28 +0200

    One file to rule them both.
    Removed RMM and reworked the other. It now has a slient switch (amount others).

--- ITGlue PS Wrapper install.ps1
+++ ITGlue PS Wrapper install.ps1
@@ -1,60 +1,81 @@
-# Code from http://www.expta.com/2017/03/how-to-self-elevate-powershell-script.html
-# Self-elevate the script if required
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        Exit
-    }
-}
+[cmdletbinding(DefaultParameterSetName="Loud")]
+param(
+	[Parameter(ParameterSetName="Loud")]
+	[Parameter(ParameterSetName="SilentInstall")]
+    [String]$APIKey = "",
+
+    [Parameter(ParameterSetName="Loud")]
+	[Parameter(ParameterSetName="SilentInstall")]
+    [Switch]$ExportAPIKey,
+
+    [Parameter(ParameterSetName="Loud")]
+	[Parameter(ParameterSetName="SilentInstall")]
+    [Alias('locale','dc')]
+    [ValidateSet( 'US', 'EU')]
+    [String]$DataCenter = '',
+
+    [Parameter(ParameterSetName="Loud")]
+	[Parameter(ParameterSetName="SilentInstall")]
+    [Switch]$SaveSettings,
+
+    [Parameter(ParameterSetName="SilentInstall")]
+    [Switch]$Silent
+)
 
 
 # Download required software
-Write-Host 'Downloading ITGlueAPI PowerShell wrapper... ' -NoNewline
+if(!$Silent) {Write-Host 'Downloading ITGlueAPI PowerShell wrapper... ' -NoNewline}
 Invoke-WebRequest 'https://codeload.github.com/itglue/powershellwrapper/zip/master' -OutFile .\ITGlueAPI.zip
-Write-Host 'Complete!'
+if(!$Silent) {Write-Host 'Complete!'}
 
 # Extract
-Write-Host "Extracting to $PWD\powershellwrapper-master... " -NoNewline
+if(!$Silent) {Write-Host 'Extracting to $PWD\powershellwrapper-master... ' -NoNewline}
 Expand-Archive -Path .\ITGlueAPI.zip -DestinationPath .\ -Force
-Write-Host 'Complete!'
+if(!$Silent) {Write-Host 'Complete!'}
 
 # Copy
-Write-Host "Coping to $env:ProgramFiles\WindowsPowerShell\Modules\ITGlueAPI... " -NoNewline
+if(!$Silent) {Write-Host 'Coping to $env:ProgramFiles\WindowsPowerShell\Modules\ITGlueAPI... ' -NoNewline}
 Copy-Item '.\powershellwrapper-master\ITGlueAPI' "$env:ProgramFiles\WindowsPowerShell\Modules\ITGlueAPI" -Recurse -Force
-Write-Host 'Complete!'
+if(!$Silent) {Write-Host 'Complete!'}
 
 # Delete items
-Write-Host "Deleting $PWD\ITGlueAPI.zip... " -NoNewline
+if(!$Silent) {Write-Host 'Deleting $PWD\ITGlueAPI.zip... ' -NoNewline}
 Remove-Item .\ITGlueAPI.zip
-Write-Host 'Complete!'
+if(!$Silent) {Write-Host 'Complete!'}
 Remove-Item .\powershellwrapper-master -Recurse
-Write-Host "Deleting $PWD\powershellwrapper-master... " -NoNewline
-Write-Host 'Complete!'
-Write-Host ''
+if(!$Silent) {Write-Host 'Deleting $PWD\powershellwrapper-master... ' -NoNewline}
+if(!$Silent) {Write-Host 'Complete!'}
+if(!$Silent) {Write-Host ''}
 
 # Import ITGlue API
-Import-Module ITGlueAPI 
+Import-Module ITGlueAPI
 
 # Add Base URI for the API
-Add-ITGlueBaseURI
+if($APIKey -ne "") {
+	Add-ITGlueAPIKey -Api_Key $APIKey
+}
 
-# First time setup
-# Write-Host "Enter your ITGlue API key when asked to."
-Add-ITGlueAPIKey
+if($DataCenter -ne "") {
+	Add-ITGlueBaseURI -data_center $DataCenter
+} else {
+	Add-ITGlueBaseURI
+}
 
 # Save API key for user
-Write-Host "Exporting settings... " -NoNewline
-Export-ITGlueModuleSettings
-Write-Host 'Complete!'
+if($SaveSettings) {
+	if(!$Silent) {Write-Host 'Exporting settings... ' -NoNewline}
+	Export-ITGlueModuleSettings
+	if(!$Silent) {Write-Host 'Complete!'}
+}
 
-$utf8fix = @'
-$ITGlue_Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$ITGlue_Headers.Add("Content-Type", 'application/vnd.api+json; charset=utf-8')
+## Commented out because change on ITGlue's side. Does not work.
+# $utf8fix = @'
+# $ITGlue_Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+# $ITGlue_Headers.Add("Content-Type", 'application/vnd.api+json; charset=utf-8')
 
-Set-Variable -Name "ITGlue_Headers"  -Value $ITGlue_Headers -Scope global
+# Set-Variable -Name "ITGlue_Headers"  -Value $ITGlue_Headers -Scope global
 
 
-Import-ITGlueModuleSettings
-'@
-$utf8fix > "$env:ProgramFiles\WindowsPowerShell\Modules\ITGlueAPI\ITGlueAPI.psm1"
+# Import-ITGlueModuleSettings
+# '@
+# $utf8fix > "$env:ProgramFiles\WindowsPowerShell\Modules\ITGlueAPI\ITGlueAPI.psm1"
